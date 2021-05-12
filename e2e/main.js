7aa8fa3c8c8c3b7a9341b3ae82ab0a17c6b92a4f
Rui Costa <ruicosta042@gmail.com>
2021-05-12 23:31:06 +0100

    tests: add e2e import tests

---
+++ e2e/main.js
@@ -0,0 +1,12 @@
+import { unhandler } from '@tuplo/unhandler';
+
+unhandler({
+  appName: 'my-app-1',
+  providers: {
+    github: {
+      user: 'tuplo',
+      repo: 'unhandler',
+      token: 'secret-token-xxxxxxx',
+    },
+  },
+});
