51a39625a1f087ed69ad1c69372788f1226f2b27
Rui Costa <ruicosta042@gmail.com>
2021-06-20 23:06:23 +0100

    feat: dual cjs/esm

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,7 @@ rimraf dist
 rimraf cjs
 tsc --build tsconfig.build.json
 
-esbuild src/index.cjs --bundle --platform=node --outfile=dist/index.cjs \
+esbuild src/index.cjs --bundle --platform=node --outfile=cjs/index.js \
   --external:node-fetch --external:https
 esbuild src/index.ts --bundle --format=esm --outfile=dist/index.js \
   --external:node-fetch --external:https
-
-# node12 compatibility
-mkdir cjs && cp dist/index.cjs cjs/index.js
