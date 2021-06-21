23b843775d72aad4e4556a2e35cab5a54d322685
Rui Costa <ruicosta042@gmail.com>
2021-06-21 17:07:56 +0100

    fix: package main file extension

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,5 +6,5 @@ tsc --build tsconfig.build.json
 
 esbuild src/index.cjs --bundle --platform=node --outfile=cjs/index.js \
   --external:node-fetch --external:https
-esbuild src/index.ts --bundle --format=esm --outfile=dist/index.js \
+esbuild src/index.ts --bundle --format=esm --outfile=dist/index.mjs \
   --external:node-fetch --external:https
