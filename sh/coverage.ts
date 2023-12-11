bead3f0cf1b1ce3a102895a568332df3b5928364
Rui Costa <ruicosta042@gmail.com>
2023-12-11 13:43:23 +0000

    chore: drop yarn, use npm

--- sh/coverage.ts
+++ sh/coverage.ts
@@ -8,7 +8,7 @@ async function main() {
 	await $`rm -rf .nyc_output/`;
 
 	const flags = ["--coverage true"];
-	await $`NODE_ENV=test LOG_LEVEL=silent nyc yarn test:ci ${flags}`;
+	await $`NODE_ENV=test LOG_LEVEL=silent nyc npm run test:ci -- ${flags}`;
 }
 
 main();
