b0ca05f011816167a9e7192808387b3b628bff63
Rui Costa <ruicosta042@gmail.com>
2024-08-24 11:38:45 +0100

    feat: use tsup

--- sh/build.ts
+++ sh/build.ts
@@ -4,29 +4,8 @@ async function main() {
 	const $ = shell.$({ verbose: true });
 
 	await $`rm -rf dist`;
-	await $`tsc --build tsconfig.build.json`;
-
-	// const modes = ["cjs", "esm"];
-	// for await (const mode of modes) {
-	// 	const flags = [
-	// 		"src/index.ts",
-	// 		"--bundle",
-	// 		`--format=${mode}`,
-	// 		"--platform=node",
-	// 		"--minify",
-	// 		`--outfile=dist/index.${mode}.js`,
-	// 	];
-
-	// 	await $`esbuild ${flags}`;
-	// }
-
-	const flags = ["--bundle", "--platform=node"];
-
-	await $`esbuild src/cjs/index.cjs --outfile=dist/index.cjs ${flags}`;
-	await $`esbuild src/index.ts --format=esm --outfile=dist/index.mjs ${flags}`;
-
-	// await $`rm dist/github.js dist/index.js`;
-	await $`rm -rf dist/helpers`;
+	await $`tsc --project tsconfig.build.json`;
+	await $`tsup src/index.ts`;
 }
 
 main();
